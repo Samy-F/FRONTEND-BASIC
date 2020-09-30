7baca214a573d81a4e74faf535e1812c5c4d44e2
Samy Farahat <Samyfarahat2003@gmail.com>
2020-09-30 02:36:24 +0200

    fixed a few bugs

--- opdracht/contact.js
+++ opdracht/contact.js
@@ -5,12 +5,15 @@ const verstuur = () => {
     let onderwerp = document.getElementById('onderwerp').value || undefined;
     let conf;
 
-    if(vnaam && anaam && email && onderwerp) {
-        let conf = confirm("weet je zeker dat je dit contact formulier wilt verzenden?")
+    if(vnaam != undefined && anaam != undefined && email != undefined && onderwerp != undefined) {
+        conf = confirm("weet je zeker dat je dit contact formulier wilt verzenden?")
     }
 
-
     if(conf){
+        console.log('succes')
         alert(`Bedankt voor je contact aanvraag ${vnaam} ${anaam}. Er wordt zo snel mogelijk contact met u opgenomen!`)
+    } else {
+        console.log('fail')
+        console.log(conf)
     }
 }
